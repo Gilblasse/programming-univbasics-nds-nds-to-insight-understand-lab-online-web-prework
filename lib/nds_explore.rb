45325e38159958f7c359534ba2b90ffb4a762d56
Nethelbert <gilblasse@gmail.com>
2019-10-24 16:04:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,23 +5,16 @@ require 'pp'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  pp nds
+  # pp nds
 end
 
 def print_first_directors_movie_titles
   pretty_db = pp directors_database
   row_index = 0
+  puts pretty_db[row_index]
 
  while row_index < pretty_db.size do
-    if pretty_db[row_index][:name] == "Stephen Spielberg"
-      movie_titles_array = pretty_db[row_index][:movies]
-      movie_row_index = 0
-      while movie_row_index < movie_titles_array.size do
-       movie = movie_titles_array[movie_row_index]
-       puts movie[:title]
-       movie_row_index += 1
-      end
-    end
+  end
     row_index += 1
  end
 end
